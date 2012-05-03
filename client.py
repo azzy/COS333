30649a2d91a5f29cb66d2923562838fc32caf2fa
root <root@domU-12-31-39-09-85-C6.compute-1.internal>
2012-05-03 13:27:10 -0400

    glue.py is now my working copy, glue_backup.py is backup from before we started editing again on Wednesday night, and /var/www/glue/glue.py is theold version + a method get_self. API better reflects the working copy than the backup

--- client.py
+++ client.py
@@ -6,56 +6,96 @@ import string
 import json
 import hmac
 import hashlib
-import urllib2
-from itertools import izip, cycle
+import urllib2, urllib
 
-key = "okXRDgXqnDfyYK11nARRIdUy5xmuGsJi00DQuyzaGYY"
-xor_key = "37xjAjWSE5pr2x6gXEsxKJMwcR7L54hXBY2bjz1UnCQmOS8Nm84ePMDCjyr7Sgt2YvP8lQRHB2gQlKehCWUMW4dPm2flhdZSwCU0LGWZdydSLS1C3OUvNsfjQPcWnuAqcQiAPMgMQf5AhIAtRmhPxNX9Gg1tUXtfMix1jJCgLkCwLiBKBJFsxq4oCUaeyfkudkc9QljacqadsF2jv9u7V3A"
-my_key = 'another-secret-key'
-host = 'http://ec2-50-17-119-54.compute-1.amazonaws.com'
-
-def make_first_digest(message):
-    ''' return a digest for the message.'''
-    return hmac.new('secret-shared-key', message, hashlib.sha1).hexdigest()
+# ---------------------------- AUTH HELPERS ------------------------------ #
+secret_key = "okXRDgXqnDfyYK11nARRIdUy5xmuGsJi00DQuyzaGYY"
 
 def make_digest(message):
     ''' return a digest for the message.'''
-    return hmac.new('hashed-password-or-uid', message, hashlib.sha1).hexdigest()
+    return hmac.new(secret_key, message, hashlib.sha224).hexdigest()
 
-def xor_crypt_string(data, key):
-    return ''.join(chr(ord(x) ^ ord(y)) for (x, y) in izip(data, cycle(key)))
+def encode(dictionary):
+    ret = ''
+    for k, v in dictionary.iteritems():
+        ret += str(k) + '+' + str(v) + '|'
+    return ret[:-1]
 
-@route('/get_users')
-def get_users():
-    method = "get_users"
-    sig = make_digest(method)
-    url = host + ':9000/?method=' + method + '&sig=' + sig
-    f = urllib2.urlopen(url)
-    m = json.loads(f.read())
-    f.close()
-    new_sig = m['sig']
-    data = m['data']
-    original = xor_crypt_string(data, key=my_key)
-    actual_digest = make_digest(original)
+def decode(s):
+    ret = {}
+    kv = s.split('|')
+    for pair in kv:
+        key, value = pair.split(' ', 1)
+        ret[key] = value
+    return ret
+
+# ------------------------------ INTERFACE ------------------------------- #
+
+req_none = ['login']
+req_key = ['get_users',
+           'create_user',
+           'get_events',
+           'get_user'
+           ]
+req_tok = ['get_auth',
+           'get_self',
+           'update_user',
+           'remove_user',
+           'get_friends',
+           'add_friend',
+           'remove_friend',
+           'get_event',
+           'remove_event',
+           'create_event',
+           'update_event',
+           'get_guests',
+           'add_guest',
+           'remove_guest',
+           'update_response'
+           ]
+token = 'bullshit'
+@route('/')
+def app():
+    global token
+    method = request.GET['method']
+    args = {}
+    args['name'] = request.GET.get('name', '')
+    args['lastname'] = request.GET.get('lastname', '')
+    args['email'] = request.GET.get('email','')
+    args['password'] = request.GET.get('password','')
+    args['eventid'] = request.GET.get('eventid', '')
+    args['friendid'] = request.GET.get('friendid', '')
+    args['category'] = request.GET.get('category', '')
+    args['location'] = request.GET.get('location', '')
+    args['starttime'] = request.GET.get('starttime', '')
+    args['endtime'] = request.GET.get('endtime', '')
+    args['description'] = request.GET.get('description','')
+    args['guestid'] = request.GET.get('guestid', '')
+    args['userid'] = request.GET.get('userid', '')
+    args['response'] = request.GET.get('response', '')
     
-    if new_sig != actual_digest:
-        return 'WARNING: Data corruption'
-    else:
-        return original
+#    sig = encode({ 'method': method, 'sig': make_digest(method) })
+    key = secret_key
+    host = 'http://localhost'
+    url = host + '/' + method
+    if method in req_tok:
+        url += '?token=' + token
+    elif method in req_key:
+        url += '?key=' + key
+    elif method in req_none:
+        url += '?'
+    else: return 'unsupported method'
 
-@route('/create_user')
-def create_user():
-    name = request.GET['name']
-    lastname = request.GET.get('lastname', '')
-    email = request.GET['email']
-    password = request.GET['password']
-    p = hashlib.md5(password).hexdigest()
-    method = "create_user"
-    args = {'name': name,
-            'lastname': lastname,
-            'email':email,
-            'password':p}
-    sig = make_digest(method)
-    url = host + ':9000/?method=' + method + '&args=' + args + '&sig=' + sig
+    for param, val in args.iteritems():
+        if len(val) > 0:
+            url += '&' + param + '=' + urllib.quote_plus(val)    
 
-run(host='ec2-50-17-119-54.compute-1.amazonaws.com',port=8085)
+    print url
+    f = urllib2.urlopen(url)
+    ret = f.read()
+    f.close()
+    if method == 'login' and ret is not 'Authentication Failed':
+        token = ret
+    return ret
+
+run(host='0.0.0.0',port=8085)
